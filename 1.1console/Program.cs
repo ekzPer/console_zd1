aa7dbbf4db06b39293bb00d6faf14aa2dd9c34c7
ekzPer <119665454+ekzPer@users.noreply.github.com>
2022-12-02 13:11:52 +0300

    Add files via upload

---
+++ 1.1console/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("s= ");
+            float s = float.Parse(Console.ReadLine());
+            double r = Math.Sqrt(s / Math.PI);
+            Console.WriteLine("r= Радиус окружности, длина которой равна единице составляет:{0:N4}", r);
+            Console.WriteLine("Для продолжения нажмите на любую клавишу . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ 1.2console/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите трехначное число");
+            int x;
+            String input = Console.ReadLine();
+            int.TryParse(input, out x);
+            int first = x / 100;
+            int second = (x % 100) / 10;
+            if (first < second)
+                Console.WriteLine("Второе число больше");
+            if (first > second)
+                Console.WriteLine("Первое число больше");
+            if (first == second)
+                Console.WriteLine("Числа равны");
+            Console.WriteLine("Для продолжения нажмите на любую клавишу . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
